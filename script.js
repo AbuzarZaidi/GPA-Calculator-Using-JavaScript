d9038380ca208955eff3aac7b320861f1c0af691
AbuzarZaidi <abuzarzaidi947@gmail.com>
2021-06-21 02:34:31 -0800

    commit

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ let flag = 0;
 let totalCredits = 0;
 let gradePoint = 0;
 // gpa.addEventListener("click", (e) => {
-const mainFunc = () => {
+const gpaFunc = () => {
   document.getElementById("mainDiv").innerHTML = ` 
     <h1>GPA Calculator</h1>
     <div class="container" id="container">
@@ -173,7 +173,7 @@ const clearFunc = () => {
   clear.addEventListener("click", (e) => {
     console.log("click");
     count = 1;
-    mainFunc();
+    gpaFunc();
     document.getElementById("show").innerHTML = "";
   });
 };
@@ -189,7 +189,159 @@ function removeWarning() {
   id.remove();
 }
 
-// cgpa.addEventListener('click',(e)=>{
-//     console.log('cgpa');
+//=======================================================================================
+//CGPA
+const cgpaFunc = () => {
+  console.log("cgpa");
+  document.getElementById("mainDiv").innerHTML = `
+    <h1>CGPA Calculator</h1>
+    <div class="container" id="container">
+    <div class="row">
+      <div class=" col-4">
+        Semester
+      </div>
+      <div class=" col-4">
+        Credits
+      </div>
+      <div class=" col-4">
+       GPA
+      </div>
+     
+    </div>
+    <hr>
+    <div class="row">
+    <div class="col-4">
+    <input type="text" class="form-control subjectName" placeholder="Semester ${count}"disabled >
+    </div>
+    <div class="col-4">
+      <input type="text" class="form-control subjectName" placeholder="credits" id="${count}credits" >
+    </div>
+    <div class="col-4">
+      <input type="text" class="form-control" placeholder="GPA"  id="${count}gpa" >
+    </div>
+    </div>
+    <div class="row mt-2">
+    <div class="col-4">
+    <input type="text" class="form-control subjectName" placeholder="Semester ${
+      count + 1
+    }"disabled >
+    </div>
+    <div class="col-4">
+      <input type="text" class="form-control subjectName" placeholder="credits" id="${
+        count + 1
+      }credits" >
+    </div>
+    <div class="col-4">
+      <input type="text" class="form-control" placeholder="GPA"  id="${
+        count + 1
+      }gpa" >
+    </div>
+    </div>
+    </div>
+    <div class="row d-flex justify-content-center"  >
+    <button class="my-4" id="add">+ ADD</button>
+    </div>
+    <div class="row row d-flex justify-content-center" >
+    <button class="my-1" id="calculate">Calculate</button>
+    <button class="my-1 mx-2" id="clear">Clear</button>
+    </div>
+    <div class="row row d-flex justify-content-center" >
+    <button class="my-1 mx-2" id="back" onclick="location.reload()">Back</button>
+    </div>
+    `;
+  count++;
+  add = document.getElementById("add");
+  calculate = document.getElementById("calculate");
+  clear = document.getElementById("clear");
+  cgpaAddFunc();
+   cgpaCalculateFunc();
+   cgpaClearFunc();
+};
 
-// })
+const cgpaAddFunc = () => {
+  console.log("click");
+
+  add.addEventListener("click", (e) => {
+    console.log("click add");
+
+    count++;
+    let container = document.getElementById("container");
+    let div = document.createElement("div");
+    div.setAttribute("class", "row mt-3");
+    div.setAttribute("id", "div");
+    let div1 = document.createElement("div");
+    div1.setAttribute("class", "col-4");
+    let input1 = document.createElement("input");
+    input1.setAttribute("type", "text");
+    input1.setAttribute("class", "form-control");
+    input1.setAttribute("id", `${count}semester`);
+    input1.setAttribute("placeholder", `Semester ${count}`);
+    input1.setAttribute("disabled", true);
+    div1.appendChild(input1);
+
+    let div2 = document.createElement("div");
+    div2.setAttribute("class", "col-4");
+    let input2 = document.createElement("input");
+    input2.setAttribute("type", "text");
+    input2.setAttribute("class", "form-control ");
+    input2.setAttribute("id", `${count}credits`);
+    input2.setAttribute("placeholder", "Credits");
+    div2.appendChild(input2);
+
+    let div3 = document.createElement("div");
+    div3.setAttribute("class", "col-4");
+    let input3 = document.createElement("input");
+    input3.setAttribute("type", "text");
+    input3.setAttribute("class", "form-control ");
+    input3.setAttribute("id", `${count}gpa`);
+    input3.setAttribute("placeholder", "GPA");
+    div3.appendChild(input3);
+
+    div.appendChild(div1);
+    div.appendChild(div2);
+    div.appendChild(div3);
+
+    container.appendChild(div);
+  });
+};
+
+//Calculate CGPA
+let tcredits = 0;
+let tgpa = 0;
+const cgpaCalculateFunc = () => {
+  calculate.addEventListener("click", (e) => {
+    flag = 0;
+    totalCredits = 0;
+    gradePoint = 0;
+    for (let i = 1; i <= count; i++) {
+      tcredits = document.getElementById(`${i}credits`).value;
+      totalCredits += parseInt(tcredits);
+      tgpa = document.getElementById(`${i}gpa`).value;
+      gradePoint += parseFloat(tcredits) * parseFloat(tgpa);
+      if (tcredits == "" || tgpa == "") {
+        flag = 1;
+        showWarning();
+        setTimeout(function () {
+          removeWarning();
+        }, 3500);
+        continue;
+      }
+    }
+    if (flag == 0) {
+      let CGPA = gradePoint / totalCredits;
+
+      console.log(CGPA);
+      let show = document.getElementById("show");
+      show.innerHTML = `<h3> your CGPA is : ${CGPA} </h3> <br> <h4> your Total Credits are : ${totalCredits} </h4>`;
+    }
+  });
+};
+
+const cgpaClearFunc = () => {
+  clear.addEventListener("click", (e) => {
+    console.log("click");
+    count = 1;
+    cgpaFunc();
+    document.getElementById("show").innerHTML = "";
+  });
+};
